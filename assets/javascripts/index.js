235f1f1562c52424328880a17211378d8e45b701
岩崎広平 <iwark02@gmail.com>
2016-12-04 04:04:46 +0900

    Add kyoumachi sample

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,1 +1,9 @@
-alert('hi');
+$(function(){
+  // http://kyoumachi-shinjuku.com/ サンプル
+  if (window.location.pathname.indexOf("kyoumachi") > -1) {
+    $('.mainImage').attr("src", "./assets/images/kyoumachi_main.png");
+    $('body').css("background", "url(./assets/images/kyoumachi_bg.jpg) center top repeat");
+    $('body').css("color", "#000");
+    $('.subMenu span').css("color", "#000");
+  }
+})
