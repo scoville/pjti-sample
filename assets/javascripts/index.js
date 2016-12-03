592bf581f0252a9f0ebb27808ef7415cdf7d5aa6
岩崎広平 <iwark02@gmail.com>
2016-12-04 04:42:34 +0900

    Fix image path

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -12,7 +12,7 @@ $(function(){
   // http://kyoumachi-shinjuku.com/ サンプル
   if (window.location.pathname.indexOf("kyoumachi") > -1) {
     $('.mainImage').attr("src", "./assets/images/kyoumachi_main.png");
-    $('body').css("background", "url(./assets/images/kyoumachi_bg.jpg) center top repeat");
+    $('body').css("background", "url(./assets/images/kyoumachi_bg.png) center top repeat");
     $('body').css("color", "#000");
     $('.subMenu span').css("color", "#000");
     $('.about .box .description').css("color", "#666");
